089db1f5001f31d0f8d81a46d756e7834448b116
roy.luo <roy55688@users.noreply.github.com>
2026-06-11 11:11:39 +0800

    Revert "更新js看變數"
    This reverts commit c9142256a3838dfe64e2da2378a81c7298d3a9dc.

--- netlify/functions/notion.js
+++ netlify/functions/notion.js
@@ -3,8 +3,7 @@ export async function handler() {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            tokenExists: !!process.env.NOTION_TOKEN,
-            databaseExists: !!process.env.NOTION_DATABASE_ID
+            message: "Netlify Function OK"
         })
     };
 }
