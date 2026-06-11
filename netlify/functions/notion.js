e9cd26daa08b2fde6e617081f719834a2291f3e5
roy.luo <roy55688@users.noreply.github.com>
2026-06-11 11:18:15 +0800

    呼叫NotionAPI

--- netlify/functions/notion.js
+++ netlify/functions/notion.js
@@ -1,10 +1,21 @@
 export async function handler() {
 
+    const response = await fetch(
+        `https://api.notion.com/v1/databases/${process.env.NOTION_DATABASE_ID}/query`,
+        {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${process.env.NOTION_TOKEN}`,
+                "Notion-Version": "2022-06-28",
+                "Content-Type": "application/json"
+            }
+        }
+    );
+
+    const data = await response.json();
+
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            tokenExists: !!process.env.NOTION_TOKEN,
-            databaseExists: !!process.env.NOTION_DATABASE_ID
-        })
+        body: JSON.stringify(data)
     };
 }
