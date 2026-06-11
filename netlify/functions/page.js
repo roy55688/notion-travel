7570227030c4c155e1c513ab1539325091ed9dde
roy.luo <roy55688@users.noreply.github.com>
2026-06-11 15:02:12 +0800

    增加page.js邏輯

--- netlify/functions/page.js
+++ netlify/functions/page.js
@@ -2,10 +2,21 @@ export async function handler(event) {
 
     const pageId = event.queryStringParameters.id;
 
+    const response = await fetch(
+        `https://api.notion.com/v1/blocks/${pageId}/children`,
+        {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
+                "Notion-Version": "2022-06-28"
+            }
+        }
+    );
+
+    const data = await response.json();
+
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            pageId
-        })
+        body: JSON.stringify(data)
     };
 }
